abdd57f1cf6ec138afec98c74fcde8a0d3fbef79
Beck Kogan <beck.kogan@sfbrightworks.org>
2024-10-10 10:46:38 -0700

    Added room class and fixed some bugs related to arrows

--- game_run.py
+++ game_run.py
@@ -9,9 +9,8 @@ infoObject = pygame.display.Info()
 
 SCREEN_WIDTH = infoObject.current_w
 SCREEN_HEIGHT = infoObject.current_h
-FPS = 144
-interactables = []
-room = 1
+FPS = 24
+room = 2
 
 # Colors
 WHITE = (255, 255, 255)
@@ -29,6 +28,8 @@ background_image1 = pygame.image.load('Room1.png')
 background_image1 = pygame.transform.scale(background_image1, (SCREEN_WIDTH, SCREEN_HEIGHT))
 background_image2 = pygame.image.load('Room2.jpg')
 background_image2 = pygame.transform.scale(background_image2, (SCREEN_WIDTH, SCREEN_HEIGHT))
+background_image3 = pygame.image.load('Room3.png')
+background_image3 = pygame.transform.scale(background_image3, (SCREEN_WIDTH, SCREEN_HEIGHT))
 #scale the image to 100x100
 left_arrow_image = pygame.image.load('left_arrow.png')
 left_arrow_image = pygame.transform.scale(left_arrow_image, (100, 100))
@@ -47,52 +48,88 @@ class Interactable:
         screen.blit(self.image, (self.x, self.y))
     
     def click_effect(self):
-        global room
         print(self.name + " was clicked!")
         # Add logic for what happens when interactable is clicked
-        if room == 1:
-            room = 2
-        elif room == 2:
-            room = 1
 
     def is_clicked(self, position):
         x, y = position
         return self.x < x < self.x + self.width and self.y < y < self.y + self.height
 
+class Arrow(Interactable):
+    def __init__(self, x, y, width, height, image, name="Interactable", direction="left"):
+        super().__init__(x, y, width, height, image, name)
+        self.direction = direction
 
-def handle_click(position):
-    x, y = position
-    print(f"Clicked at position: {x}, {y}")
-    # Add logic to handle clicks here
-    for interactable in interactables:
-        if interactable.is_clicked(position):
-            interactable.click_effect()
+    def click_effect(self):
+        global room
+        print(self.name + " was clicked!")
+        # Add logic for what happens when interactable is clicked
+        if self.direction == "left":
+            if room == 2 or 3:
+                print("jabba1")
+                room -= 1
+            else:
+                print("jabba2")
+                pass
+        elif self.direction == "right":
+            if room == 1 or 2:
+                print("jabba3")
+                room += 1
+            else:
+                print("jabba4") 
+                pass
+
+class Room:
+    def __init__(self, background_image, interactables):
+        self.background_image = background_image
+        self.interactables = interactables
+
+    def draw(self):
+        screen.blit(self.background_image, (0, 0))
+        for interactable in self.interactables:
+            interactable.draw()
+
+    def handle_click(self, interactables, position):
+        x, y = position
+        print(f"Clicked at position: {x}, {y}")
+        # Add logic to handle clicks here
+        for interactable in interactables:
+            if interactable.is_clicked(position):
+                interactable.click_effect()
 
 # Game loop
 def game_loop():
-    global interactables
-    interactables = []
-    left_arrow = Interactable(100, SCREEN_HEIGHT/2-50, 100, 100, left_arrow_image, "Left Arrow")
-    right_arrow = Interactable(SCREEN_WIDTH-200, SCREEN_HEIGHT/2-50, 100, 100, right_arrow_image, "Right Arrow")
-    interactables.append(left_arrow)
-    interactables.append(right_arrow)
+    left_arrow = Arrow(100, SCREEN_HEIGHT/2-50, 100, 100, left_arrow_image, "Left Arrow", "left")
+    right_arrow = Arrow(SCREEN_WIDTH-200, SCREEN_HEIGHT/2-50, 100, 100, right_arrow_image, "Right Arrow", "right")
+    main_room1 = Room(background_image1, [])
+    left_room1 = Room(background_image2, [])
+    right_room1 = Room(background_image3, [])
+    main_room1.interactables.append(left_arrow)
+    main_room1.interactables.append(right_arrow)
+    left_room1.interactables.append(right_arrow)
+    right_room1.interactables.append(left_arrow)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT or event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
                 pygame.quit()
                 sys.exit()
             elif event.type == pygame.MOUSEBUTTONDOWN:
-                handle_click(event.pos)
+                if room == 2:
+                    main_room1.handle_click(main_room1.interactables, event.pos)
+                elif room == 1:
+                    left_room1.handle_click(left_room1.interactables, event.pos)
+                elif room == 3:
+                    right_room1.handle_click(right_room1.interactables, event.pos)
 
         # Draw everything
         screen.fill(WHITE)
-        if room == 1:
-            screen.blit(background_image1, (0, 0))
-        elif room == 2:
-            screen.blit(background_image2, (0, 0))
-        
-        for interactable in interactables:
-            interactable.draw()
+        if room == 2:
+            main_room1.draw()
+        elif room == 1:
+            left_room1.draw()
+        elif room == 3:
+            right_room1.draw()
 
         # Update the display
         pygame.display.flip()
